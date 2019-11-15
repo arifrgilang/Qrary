a4e32285cb37622f7d3192fca38659950b74e7e2
Arif R Gilang <arifrgilang@gmail.com>
2019-11-15 09:53:40 +0700

    Add animation to splashscreen

--- app/src/main/java/com/rz/qrary/SplashActivity.kt
+++ app/src/main/java/com/rz/qrary/SplashActivity.kt
@@ -4,6 +4,11 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
+import android.view.View
+import android.view.animation.AnimationUtils
+import androidx.core.app.ActivityCompat
+import androidx.core.app.ActivityOptionsCompat
+import kotlinx.android.synthetic.main.activity_splash.*
 
 class SplashActivity : AppCompatActivity() {
 
@@ -11,9 +16,18 @@ class SplashActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
 
+        val fadeAnim = AnimationUtils.loadAnimation(this, R.anim.fade_in)
+        logo_splash.startAnimation((fadeAnim))
+
         Handler().postDelayed({
-            startActivity(Intent(this, LoginActivity::class.java))
-            finish()
+            val intent =  Intent(this, LoginActivity::class.java)
+//            intent.putExtra(LoginActivity.EXT)
+            val options = ActivityOptionsCompat
+                .makeSceneTransitionAnimation(this, logo_splash, "qrary_logo")
+
+            startActivity(intent, options.toBundle())
+            ActivityCompat.finishAfterTransition(this)
+//            finish()
         }, 2000)
     }
 }
